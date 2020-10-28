4e856fd074ae7aaeab0ecdd1ca58242acec1c524
nikolina424 <inina007@gmail.com>
2020-10-28 14:01:59 +0100

    auth service configuration and security

---
+++ auth-service/src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.security.TokenUtils;
+import javassist.NotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth-service")
+public class AuthController {
+
+    private final TokenUtils _tokenUtils;
+
+    public AuthController(TokenUtils tokenUtils) {
+        _tokenUtils = tokenUtils;
+    }
+
+    @GetMapping("/verify")
+    public String verify(@RequestHeader("Auth-Token") String token) throws NotFoundException {
+        return _tokenUtils.getUsernameFromToken(token);
+    }
+}
---
+++ zuul/src/main/java/com/example/demo/IAuthClient.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "auth")
+public interface IAuthClient {
+
+    @GetMapping("/auth/verify")
+    String verify(@RequestHeader("Auth-Token") String token);
+
+    @GetMapping("/auth/permission")
+    String getPermission(@RequestHeader("Auth-Token") String token);
+
+}
